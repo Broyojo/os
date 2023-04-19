1ffbe26ef6f5750fe7664c48398be61b8a2e153b
Broyojo <dha@xoba.com>
2023-04-18 23:02:34 -0400

    made color palette nicer

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,10 @@
 
 mod vga_buffer;
 
-use core::panic::PanicInfo;
+use core::{
+    ops::{Add, Mul},
+    panic::PanicInfo,
+};
 
 use crate::vga_buffer::WRITER;
 
@@ -21,19 +24,19 @@ pub extern "C" fn _start() -> ! {
     const COLOR_PALETTE: [vga_buffer::Color; 16] = [
         vga_buffer::Color::Black,
         vga_buffer::Color::Blue,
-        vga_buffer::Color::Green,
-        vga_buffer::Color::Cyan,
-        vga_buffer::Color::Red,
-        vga_buffer::Color::Magenta,
-        vga_buffer::Color::Brown,
-        vga_buffer::Color::LightGray,
-        vga_buffer::Color::DarkGray,
         vga_buffer::Color::LightBlue,
-        vga_buffer::Color::LightGreen,
+        vga_buffer::Color::Cyan,
         vga_buffer::Color::LightCyan,
-        vga_buffer::Color::LightRed,
-        vga_buffer::Color::Pink,
+        vga_buffer::Color::Green,
+        vga_buffer::Color::LightGreen,
+        vga_buffer::Color::Brown,
         vga_buffer::Color::Yellow,
+        vga_buffer::Color::DarkGray,
+        vga_buffer::Color::LightGray,
+        vga_buffer::Color::Magenta,
+        vga_buffer::Color::Pink,
+        vga_buffer::Color::LightRed,
+        vga_buffer::Color::Red,
         vga_buffer::Color::White,
     ];
 
@@ -62,28 +65,48 @@ pub extern "C" fn _start() -> ! {
     panic!("End of _start reached");
 }
 
-const MAX_ITER: usize = 100;
+const MAX_ITER: usize = 128;
 
+#[derive(Debug, Clone, Copy)]
 struct Complex {
     real: f64,
     imag: f64,
 }
 
-fn mandelbrot(point: Complex) -> usize {
-    let mut z = Complex {
-        real: 0.0,
-        imag: 0.0,
-    };
+impl Complex {
+    fn new(real: f64, imag: f64) -> Complex {
+        Complex { real, imag }
+    }
+
+    fn abs(self) -> f64 {
+        self.real * self.real + self.imag * self.imag
+    }
+}
+
+impl Add for Complex {
+    type Output = Complex;
+
+    fn add(self, other: Complex) -> Complex {
+        Complex::new(self.real + other.real, self.imag + other.imag)
+    }
+}
+
+impl Mul for Complex {
+    type Output = Complex;
+
+    fn mul(self, other: Complex) -> Complex {
+        Complex::new(
+            self.real * other.real - self.imag * other.imag,
+            self.real * other.imag + self.imag * other.real,
+        )
+    }
+}
+
+fn mandelbrot(c: Complex) -> usize {
+    let mut z = Complex::new(0.0, 0.0);
     for i in 0..MAX_ITER {
-        let z2 = Complex {
-            real: z.real * z.real - z.imag * z.imag,
-            imag: 2.0 * z.real * z.imag,
-        };
-        z = Complex {
-            real: z2.real + point.real,
-            imag: z2.imag + point.imag,
-        };
-        if z.real * z.real + z.imag * z.imag > 4.0 {
+        z = z * z + c;
+        if z.abs() > 4.0 {
             return i;
         }
     }
